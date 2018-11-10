6a4bb05c2475a7f81b0d5818fbf98b57c77f1dfd
comedian <komedian@bk.ru>
2018-11-10 18:29:38 +0500

    added example solution

--- examples/Examples/Examples/Program.cs
+++ examples/Examples/Examples/Program.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using ImageManipulate;
+using System.Drawing;
 
 namespace Examples
 {
@@ -9,6 +7,40 @@ namespace Examples
     {
         static void Main(string[] args)
         {
+
+            Bitmap base_image = (Bitmap)Image.FromFile("test_image.jpg");
+
+            // Обычный медианный фильтр
+            Bitmap median_image = Filter.MedianFilter(base_image, 3);
+            median_image.Save("MedianFilter_image.jpg");
+
+            // Картинку в серый цвет
+            Bitmap gray_image = Filter.ImageToGray(base_image);
+            gray_image.Save("gray_image.jpg");
+
+            // Размытие матрицами
+            double koef = 1d / 16d;
+            Bitmap gaus_image = Filter.ApplySmoothMethod(gray_image, Matrix.Gauss3x3, koef);
+            gaus_image.Save("gausBlur_image.jpg");
+            
+            // Доп. Массивы для нахождений линий на изображении
+            int[,] hough_array;
+            double[,] atans;
+
+            //Применения оператора Собеля
+            Bitmap sobel_image = Filter.ApplySobelMethod(gaus_image, Matrix.SobelVertical, Matrix.SobelGorizontal, out atans);
+            Bitmap nonmax_image = Filter.NonMaximum(sobel_image, atans);
+            nonmax_image.Save("nonmax_image.jpg");
+
+            //постобработка изображения
+            Bitmap doubletres_image = Filter.ApplyDoubleTresholding(nonmax_image, 0.3d, 0.7d);
+            Filter.PostTreatment(doubletres_image);
+            doubletres_image.Save("doubletres_image.jpg");
+
+            //Алгоритм Хафа для нахождени линий
+            Bitmap h_image = Filter.ApplyHoughAlgoritm(doubletres_image, out hough_array);
+            Filter.DrawingLineOnImage(base_image, hough_array, 90);
+            base_image.Save("base_img_with_lines.jpg");
 
         }
     }
--- files/Matrix.cs
+++ files/Matrix.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace ImageManipulate
+﻿namespace ImageManipulate
 {
     public struct Matrix
     {
